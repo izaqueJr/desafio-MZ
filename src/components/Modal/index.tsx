ab222d7cb77bfa6f208dc17573125b42d4ea271b
Izaque Rodrigues <izaquesjunior@gmail.com>
2023-03-15 23:30:12 -0300

    feat: finaliza modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -28,32 +28,41 @@ const Modal = () => {
               Fechar
             </button>
           </Dialog.Close>
-
-          <form className="dialog__form">
-            <img src="/images/icons/newsletter-mail-icon.png" alt="" />
-            <Dialog.Title className="dialog__title">
-              Bem Vindo à MAEZTRA
-            </Dialog.Title>
-            <Dialog.Description className="dialog__description">
-              Receba em Primeira mão
-              <br />
-              <span>desconto e ofertas exclusivas </span>
-            </Dialog.Description>
-            <div className="dialog__input-container">
-              <input
-                name="email"
-                placeholder="Digite seu e-mail"
-                className="dialog__email"
-              />{" "}
+          <section className="dialog__section">
+            <div className="dialog__banner desktop">
+              <img
+                src="/images/banner-modal.png"
+                alt="banner modal"
+                style={{ height: "100%" }}
+              />
             </div>
 
-            <div className="dialog__button-container">
-              <button className="dialog__send-button">
-                <span>Enviar</span>
-                <img src="/images/icons/icon-send.png" alt="" />
-              </button>
-            </div>
-          </form>
+            <form className="dialog__form">
+              <img src="/images/icons/newsletter-mail-icon.png" alt="" />
+              <Dialog.Title className="dialog__title">
+                Bem Vindo à MAEZTRA
+              </Dialog.Title>
+              <Dialog.Description className="dialog__description">
+                Receba em Primeira mão
+                <br />
+                <span>desconto e ofertas exclusivas </span>
+              </Dialog.Description>
+              <div className="dialog__input-container">
+                <input
+                  name="email"
+                  placeholder="Digite seu e-mail"
+                  className="dialog__email"
+                />{" "}
+              </div>
+
+              <div className="dialog__button-container">
+                <button className="dialog__send-button">
+                  <span>Enviar</span>
+                  <img src="/images/icons/icon-send.png" alt="" />
+                </button>
+              </div>
+            </form>
+          </section>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
